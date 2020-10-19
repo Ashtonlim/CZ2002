659d3bccfdbb62702c4b53ffbc1cfae81e5c5269
GK <Guat Kwan@DESKTOP-0B6495K>
2020-10-19 19:48:54 +0800

    added classes class and removed test class

---
+++ src/main/java/Classes.java
@@ -0,0 +1,46 @@
+public class Classes {
+
+	private String type;
+	private String timeStart;
+	private String timeEnd;
+	private String venue;
+	
+	public Classes(String type, String timeStart, String timeEnd, String venue) {
+		this.type = type;
+		this.timeStart = timeStart;
+		this.timeEnd = timeEnd;
+		this.venue = venue;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public String getTimeStart() {
+		return timeStart;
+	}
+	
+	public String getTimeEnd() {
+		return timeEnd;
+	}
+	
+	public String getVenue() {
+		return venue;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public void setTimeStart(String timeStart) {
+		this.timeStart = timeStart;
+	}
+	
+	public void setTimeEnd(String timeEnd) {
+		this.timeEnd = timeEnd;
+	}
+	
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+}
