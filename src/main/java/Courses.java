5bc5652876ad99164d2ae334c889da515e0e9118
Ming <chongzheming98@gmail.com>
2020-10-19 13:44:30 +0800

    added Courses

---
+++ src/main/java/Courses.java
@@ -0,0 +1,31 @@
+public class Courses {
+    private Index[] indexLst;
+    private String courseCode;
+    private String school;
+    private String subjectType;
+    private String classType;
+    private int AU;
+    private int courseVacancy;
+
+    public Courses(Index[] indexLst, String courseCode, String school, String subjectType, String classType, int AU, int courseVacancy) {
+        this.indexLst = indexLst;
+        this.courseCode = courseCode;
+        this.school = school;
+        this.subjectType = subjectType;
+        this.classType = classType;
+        this.AU = AU;
+        this.courseVacancy = courseVacancy;
+    }
+
+    public int getVacancy() {
+        return courseVacancy;
+    }
+
+    public void setVacancy(int vacancy) {
+        this.courseVacancy = vacancy;
+    }
+}
+
+
+
+
