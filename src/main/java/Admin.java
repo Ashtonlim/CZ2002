17e23e679fdb289618aeafc27da261d76541364e
Lillianlyb <62116997+Lillianlyb@users.noreply.github.com>
2020-11-18 15:54:13 +0800

    Update Admin.java

--- src/main/java/Admin.java
+++ src/main/java/Admin.java
@@ -2,8 +2,8 @@ import java.io.Serializable;
 
 public class Admin extends User implements Serializable {
 
-    public Admin(String username, String password, String fullName, String gender){
-        super(username, password, fullName, gender);
+    public Admin(String username, String password, String fullName, String gender, String nationality){
+        super(username, password, fullName, gender, nationality);
     }
 
 }
